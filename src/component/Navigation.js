7bb1a0fea1c86f1f431fea816597dbb799b47d9e
Ken Lin <kenlin2809@gmail.com>
2024-11-05 00:52:23 -0500

    clicking hackviolet on navbar will now send you back to the top

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "../style/Navigation.scss";
 import Logo from "../asset/image/logo.png";
+import LogoText from "../asset/image/hackviolet_text.png";
 
 const Navigation = () => {
     return (
         <div className="navigation">
             <div className="bar">
                 <div className="logo">
-                    <a href="/">
-                        <img src={Logo} alt="HackViolet 2025 Logo" />
-                        <p>HackViolet</p>
+                    <a href="#landing">
+                        <img className="icon" src={Logo} alt="HackViolet 2025 Logo" />
+                        <img className="text" src={LogoText} alt="HackViolet 2025 Text" />
+                        {/* <p>HackViolet</p> */}
                     </a>
                 </div>
                 <div className="links">
