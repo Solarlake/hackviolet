9a15662b8b0155dc55bd38a80c2c69c44e1783d7
annieseinna <anniettran9@gmail.com>
2025-01-30 22:14:34 +0100

    Added schedule filters

--- src/component/Schedule.js
+++ src/component/Schedule.js
@@ -12,6 +12,15 @@ const eventTypeColors = {
 
 const Schedule = () => {
     const [selectedDay, setSelectedDay] = useState("Saturday");
+    const [selectedEventType, setSelectedEventType] = useState("All");
+
+    const handleEventTypeFilter = (type) => {
+        setSelectedEventType(type);
+    };
+
+    const filteredEvents = ScheduleData[selectedDay.toLowerCase()].filter((event) => {
+        return selectedEventType === "All" || event.type === selectedEventType;
+    });
 
     return (
         <div className="schedule">
@@ -27,34 +36,53 @@ const Schedule = () => {
                 ))}
             </div>
 
+            {/* Filter buttons */}
+            <div className="event_filters">
+                {["All", "HackViolet", "Club", "Meal", "MLH"].map((eventType) => (
+                    <button
+                        key={eventType}
+                        className={`filter_button ${selectedEventType === eventType ? "active" : ""} ${eventType}`}
+                        onClick={() => handleEventTypeFilter(eventType)}
+                    >
+                        {eventType}
+                    </button>
+                ))}
+            </div>
+
             <div className="event_list">
                 <div className="scrollable_content">
-                    {ScheduleData[selectedDay.toLowerCase()].map((event) => (
-                        <div key={event.id} className="event_item">
-                            <div className="event_time">
-                                <p>{event.startTime}{event.endTime && ` - ${event.endTime}`}</p>
-                            </div>
-                            <div className="event_details">
-                                <h4>{event.title}</h4>
-                                {event.location && (
-                                    <p className="event_location">
-                                        <FaMapMarkerAlt className="location_icon" /> {event.location}
-                                    </p>
-                                )}
-                                {event.type && eventTypeColors[event.type] && (
-                                    <div
-                                        className="event_type_marker"
-                                        style={{
-                                            borderColor: eventTypeColors[event.type],
-                                            color: eventTypeColors[event.type],
-                                        }}
-                                    >
-                                        {event.type}
-                                    </div>
-                                )}
-                            </div>
+                    {filteredEvents.length === 0 ? (
+                        <div className="event_item">
+                            <p>Nothing here!</p>
                         </div>
-                    ))}
+                    ) : (
+                        filteredEvents.map((event) => (
+                            <div key={event.id} className="event_item">
+                                <div className="event_time">
+                                    <p>{event.startTime}{event.endTime && ` - ${event.endTime}`}</p>
+                                </div>
+                                <div className="event_details">
+                                    <h4>{event.title}</h4>
+                                    {event.location && (
+                                        <p className="event_location">
+                                            <FaMapMarkerAlt className="location_icon" /> {event.location}
+                                        </p>
+                                    )}
+                                    {event.type && eventTypeColors[event.type] && (
+                                        <div
+                                            className="event_type_marker"
+                                            style={{
+                                                borderColor: eventTypeColors[event.type],
+                                                color: eventTypeColors[event.type],
+                                            }}
+                                        >
+                                            {event.type}
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        ))
+                    )}
                 </div>
             </div>
         </div>
