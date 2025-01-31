5bf13cd0b000b8c7f8d55c4f29c3a8fcc708472b
Ken Lin <kenlin2809@gmail.com>
2025-01-31 05:25:18 -0500

    sponsor tiers

--- src/component/Sponsors.js
+++ src/component/Sponsors.js
@@ -13,30 +13,47 @@ import mcDeanLogo from "../asset/image/logos/mc-dean.jpg";
 const Sponsors = () => {
     return (
         <div className="sponsors_container">
-            <div className="logo_container">
-                <a href="https://www.arcfield.com/" target="_blank" rel="noopener noreferrer">
-                    <img src={arcfieldLogo} alt="Arcfield Logo" className="company_logo" />
-                </a>
-                <a href="https://www.mcdean.com/" target="_blank" rel="noopener noreferrer">
-                    <img src={mcDeanLogo} alt="MC Dean Logo" className="company_logo" />
-                </a>
-                <a href="https://www.capitalone.com/" target="_blank" rel="noopener noreferrer">
-                    <img src={capitalOneLogo} alt="Capital One Logo" className="company_logo" />
-                </a>
-                <a href="https://apex.vt.edu/" target="_blank" rel="noopener noreferrer">
-                    <img src={apexLogo} alt="Apex Logo" className="company_logo" />
-                </a>
-                <a href="https://www.convergint.com/" target="_blank" rel="noopener noreferrer">
-                    <img src={convergintLogo} alt="Convergint Logo" className="company_logo" />
-                </a>
-                <a href="https://cyberinitiative.org/" target="_blank" rel="noopener noreferrer">
-                    <img src={cciLogo} alt="CCI Logo" className="company_logo" />
-                </a>
-                <a href="https://www.codepath.org/" target="_blank" rel="noopener noreferrer">
-                    <img src={codePathLogo} alt="CodePath Logo" className="company_logo" />
-                </a>
+            <div className="tier_container">
+                <div className="tier orchid">
+                    <p>Orchid</p>
+                    <div className="logo_container">
+                        <a href="https://www.arcfield.com/" target="_blank" rel="noopener noreferrer">
+                            <img src={arcfieldLogo} alt="Arcfield Logo" className="company_logo" />
+                        </a>
+                    </div>
+                </div>
+
+                <div className="tier lilac">
+                    <p>Lilac</p>
+                    <div className="logo_container">
+                        <a href="https://www.mcdean.com/" target="_blank" rel="noopener noreferrer">
+                            <img src={mcDeanLogo} alt="MC Dean Logo" className="company_logo" />
+                        </a>
+                        <a href="https://www.capitalone.com/" target="_blank" rel="noopener noreferrer">
+                            <img src={capitalOneLogo} alt="Capital One Logo" className="company_logo" />
+                        </a>
+                        <a href="https://apex.vt.edu/" target="_blank" rel="noopener noreferrer">
+                            <img src={apexLogo} alt="Apex Logo" className="company_logo" />
+                        </a>
+                    </div>
+                </div>
+
+                <div className="tier lavender">
+                    <p>Lavender</p>
+                    <div className="logo_container">
+                        <a href="https://www.convergint.com/" target="_blank" rel="noopener noreferrer">
+                            <img src={convergintLogo} alt="Convergint Logo" className="company_logo" />
+                        </a>
+                        <a href="https://cyberinitiative.org/" target="_blank" rel="noopener noreferrer">
+                            <img src={cciLogo} alt="CCI Logo" className="company_logo" />
+                        </a>
+                        <a href="https://www.codepath.org/" target="_blank" rel="noopener noreferrer">
+                            <img src={codePathLogo} alt="CodePath Logo" className="company_logo" />
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div className="copy">
+            {/* <div className="copy">
                 <p>With over 500 expected participants attending this in-person hackathon, we need your help to create an incredible experience for young, future innovators!</p>
                 <p>
                 <strong>Benefits of Sponsorship:</strong>
@@ -48,7 +65,7 @@ const Sponsors = () => {
                     </ul>
                 </p>
             </div>
-            <Button href="https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:1c24652c-dbd8-45ee-afc9-156c8ebc9852" newTab text="Sponsorship Packet"/>
+            <Button href="https://acrobat.adobe.com/id/urn:aaid:sc:VA6C2:1c24652c-dbd8-45ee-afc9-156c8ebc9852" newTab text="Sponsorship Packet"/> */}
         </div>
     );
 }
